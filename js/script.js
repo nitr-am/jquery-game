37a3510a102de0422589ba43e9dcc84c613ccd0b
Ubuntu <ubuntu@ip-172-31-86-173.ec2.internal>
2019-09-18 12:09:24 +0000

    initial commit

---
+++ js/script.js
@@ -0,0 +1,39 @@
+$(document).ready(function(){
+
+//   ----- A un panel le doy click desaparecen los hermanos    
+//   $(".container").click(function(){
+//         $(this).siblings().hide();
+//   })
+
+//   ----- Solo el que le doy click desaparece
+//   $(".theButton").click(function(){
+//         $(this).hide();
+//   })
+   
+// --------- Toco un panel, los hermanos se opadecen. 
+// -------- Toco el grande, todos vuelven a normalidad. 
+//   $(".container").click(function() {
+//         $(this).siblings().fadeTo(1000, 0.2)
+//   });
+//   $(".superButton").click(function() {
+//         $(this).next().children().children().fadeTo(1000, 1)
+//   });
+
+
+//  ------------ Si toco un panel, se oscurece,
+//   $(".theButton").mouseenter(function(){
+// 	$(this).addClass("makeBlack");
+//   });
+//   $(".theButton").mouseleave(function(){
+// 	$(this).removeClass("makeBlack");
+//   });
+
+
+// Aqui estoy poniendo un texto del color del bloque que toque al superButton, para eso me toco 
+// Poner una variable a la cual es la que le voy a coger despues el valor
+    // $(".theButton").click(function() {
+    //     var col = $(this).css("background-color")
+    //     $(".superButton").text(col);
+    // });
+   
+});
